29f8efdd00918b277805433ff0c359d1854497f3
qaqcatz <qaqcatz@gmail.com>
2022-09-20 00:03:14 +0800

    add adclibdoc

---
+++ adclibdoc.go
@@ -0,0 +1,6 @@
+// Provide stable communication interfaces with avd, which encapsulates adb and http request forwarding.
+//
+// Avoid communication failures caused by device offline and forwarding shutdown as much as possible.
+//
+// Support timeout.
+package adclib
